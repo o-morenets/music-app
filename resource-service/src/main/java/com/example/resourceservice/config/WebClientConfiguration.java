0bfb43a60ea80cda51bde1bf02eb4859abebdc5e
Oleksii Morenets <oleksii_morenets@epam.com>
2023-11-30 17:30:10 +0100

    Implement docker-compose

--- resource-service/src/main/java/com/example/resourceservice/config/WebClientConfiguration.java
+++ resource-service/src/main/java/com/example/resourceservice/config/WebClientConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfiguration {
 
-	@Value("${music-app.service.song-service.url}")
+	@Value("${music-app.service.songs.url}")
 	private String songApiUrl;
 
 	@Bean
