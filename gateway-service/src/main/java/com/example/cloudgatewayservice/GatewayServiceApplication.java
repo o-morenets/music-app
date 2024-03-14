1dcfe8f9593e889edab08469e5b9a3c01853fd34
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-03-14 14:05:02 +0100

    Fix port number for local profile

--- gateway-service/src/main/java/com/example/cloudgatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/cloudgatewayservice/GatewayServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudGatewayServiceApplication {
+public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudGatewayServiceApplication.class, args);
+		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
 }
